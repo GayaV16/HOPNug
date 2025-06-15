5f0e6875fab7cf4ab335035bf5bbbdf62af4fb21
gayatri vaidya <gayatripg16@gmail.com>
2025-06-15 16:59:55 +0200

    commit

--- test_InvoiceUpload.py
+++ test_InvoiceUpload.py
@@ -8,11 +8,12 @@ from selenium.webdriver.support.wait import WebDriverWait
 @pytest.mark.positiveflow
 def test_validfile_png(driver):
     driver = driver
-    driver.find_element(By.ID,"file").send_keys("C:\\Users\\Paras\\Downloads\\Testing\\invoice1.png")
+    driver.find_element(By.ID,"file").send_keys("C:\\Users\\Paras\\PycharmProjects\\pythonProject\\HOPN ug\\Datafile\\invoice1.png")
     driver.find_element(By.XPATH,"//input[@type='submit']").click()
     wait = WebDriverWait (driver,10)
     wait.until(expected_conditions.visibility_of_element_located((By.ID,"success-message")))
-    driver.save_screenshot("testcase1.png")
+    path = "C:\\Users\\Paras\\PycharmProjects\\pythonProject\\HOPN ug\\Invoice_testcases_screenshots\\testcase1.png"
+    driver.save_screenshot(path)
     message = driver.find_element(By.ID,"success-message").text
     print(message)
     assert "successfully" in message
@@ -20,61 +21,66 @@ def test_validfile_png(driver):
 @pytest.mark.positiveflow
 def test_validfile_jpg(driver):
     driver = driver
-    driver.find_element(By.ID, "file").send_keys("C:\\Users\\Paras\\Downloads\\Testing\\invoice2.jpg")
+    driver.find_element(By.ID, "file").send_keys("C:\\Users\\Paras\\PycharmProjects\\pythonProject\\HOPN ug\\Datafile\\invoice2.jpg")
     driver.find_element(By.XPATH, "//input[@type='submit']").click()
     wait = WebDriverWait(driver, 10)
     wait.until(expected_conditions.visibility_of_element_located((By.ID,"success-message")))
     message = driver.find_element(By.ID,"success-message").text
     print(message)
-    driver.save_screenshot('testcase2.png')
+    path = "C:\\Users\\Paras\\PycharmProjects\\pythonProject\\HOPN ug\\Invoice_testcases_screenshots\\testcase2.png"
+    driver.save_screenshot(path)
     assert "successfully" in message
 
 @pytest.mark.positiveflow
 def test_validfile_pdf(driver):
     driver = driver
-    driver.find_element(By.ID, "file").send_keys("C:\\Users\\Paras\\Downloads\\Testing\\invoice3.pdf")
+    driver.find_element(By.ID, "file").send_keys("C:\\Users\\Paras\\PycharmProjects\\pythonProject\\HOPN ug\\Datafile\\invoice3.pdf")
     driver.find_element(By.XPATH, "//input[@type='submit']").click()
     wait = WebDriverWait(driver, 10)
     wait.until(expected_conditions.visibility_of_element_located((By.ID,"success-message")))
     message = driver.find_element(By.ID,"success-message").text
     print(message)
-    driver.save_screenshot('testcase3.png')
+    path = "C:\\Users\\Paras\\PycharmProjects\\pythonProject\\HOPN ug\\Invoice_testcases_screenshots\\testcase3.png"
+    driver.save_screenshot(path)
     assert "successfully" in message
 
 @pytest.mark.positiveflow
 def test_valid_size(driver):
     driver = driver
-    driver.find_element(By.ID, "file").send_keys("C:\\Users\\Paras\\Downloads\\Testing\\invoice4.jpeg")
+    driver.find_element(By.ID, "file").send_keys("C:\\Users\\Paras\\PycharmProjects\\pythonProject\\HOPN ug\\Datafile\\invoice4.jpeg")
     driver.find_element(By.XPATH, "//input[@type='submit']").click()
     wait = WebDriverWait(driver, 10)
     wait.until(expected_conditions.visibility_of_element_located((By.ID,"success-message")))
     message = driver.find_element(By.ID,"success-message").text
     print(message)
-    driver.save_screenshot('testcase4.png')
+    path = "C:\\Users\\Paras\\PycharmProjects\\pythonProject\\HOPN ug\\Invoice_testcases_screenshots\\testcase4.png"
+    driver.save_screenshot(path)
     assert "successfully" in message
 
 @pytest.mark.negativeflow
 def test_invalidfile_xlxs(driver):
     driver = driver
-    driver.find_element(By.ID, "file").send_keys("C:\\Users\\Paras\\Downloads\\Testing\\test.xlsx")
+    driver.find_element(By.ID, "file").send_keys("C:\\Users\\Paras\\PycharmProjects\\pythonProject\\HOPN ug\\Datafile\\test.xlsx")
     driver.find_element(By.XPATH, "//input[@type='submit']").click()
     wait = WebDriverWait(driver, 10)
     wait.until(expected_conditions.visibility_of_element_located((By.ID, "error-message")))
     message = driver.find_element(By.ID, "error-message").text
     print(message)
-    driver.save_screenshot('testcase5.png')
+    path = "C:\\Users\\Paras\\PycharmProjects\\pythonProject\\HOPN ug\\Invoice_testcases_screenshots\\testcase5.png"
+    driver.save_screenshot(path)
     assert "Only PNG, JPG, or PDF files are allowed." in message
 
 @pytest.mark.negativeflow
 def test_invalid_size(driver):
     driver = driver
-    driver.find_element(By.ID, "file").send_keys("C:\\Users\\Paras\\Downloads\\Testing\\test5.jpg")
+    driver.find_element(By.ID, "file").send_keys("C:\\Users\\Paras\\PycharmProjects\\pythonProject\\HOPN ug\\Datafile\\test5.jpg")
     driver.find_element(By.XPATH, "//input[@type='submit']").click()
     wait = WebDriverWait(driver, 10)
     wait.until(expected_conditions.visibility_of_element_located((By.ID, "error-message")))
     message = driver.find_element(By.ID, "error-message").text
     print(message)
-    driver.save_screenshot('testcase6.png')
+    path = "C:\\Users\\Paras\\PycharmProjects\\pythonProject\\HOPN ug\\Invoice_testcases_screenshots\\testcase6.png"
+    driver.save_screenshot(path)
     assert "smaller than 5MB." in message
 
 
--- test_form.py
+++ test_form.py
@@ -8,77 +8,88 @@ from selenium.webdriver.support.select import Select
 
 def test_valid_inputs(driver,fill_and_submit_form):
     fill_and_submit_form(vendor="Test",amount="50",date="14/06/2025",status="Paid")
-    driver.save_screenshot("form_testcase1.png")
+    path = "C:\\Users\\Paras\\PycharmProjects\\pythonProject\\HOPN ug\\Form_testcases_screenshots\\form_testcase1.png"
+    driver.save_screenshot(path)
     message = driver.find_element(By.ID,"success-box").text
     print(message)
     assert "Form submitted successfully!" in message
 
 def test_invalid_name(driver,fill_and_submit_form):
     fill_and_submit_form(vendor="Test$%^^", amount="50", date="14/06/2025", status="Paid")
-    driver.save_screenshot("form_testcase2.png")
+    path = "C:\\Users\\Paras\\PycharmProjects\\pythonProject\\HOPN ug\\Form_testcases_screenshots\\form_testcase2.png"
+    driver.save_screenshot(path)
     message = driver.find_element(By.CLASS_NAME, "error").text
     print(message)
     assert "Vendor name must use letters and spaces only." in message
 
 def test_blank_name(driver,fill_and_submit_form):
     fill_and_submit_form(vendor=" ", amount="50", date="14/06/2025", status="Paid")
-    driver.save_screenshot("form_testcase3.png")
+    path = "C:\\Users\\Paras\\PycharmProjects\\pythonProject\\HOPN ug\\Form_testcases_screenshots\\form_testcase3.png"
+    driver.save_screenshot(path)
     message = driver.find_element(By.ID, "vendor-error").text
     print(message)
     assert "Vendor name cannot be empty." in message
 
 def test_negative_amount(driver,fill_and_submit_form):
     fill_and_submit_form(vendor="Test",amount="-50",date="14/06/2025",status="Paid")
-    driver.save_screenshot("form_testcase4.png")
+    path = "C:\\Users\\Paras\\PycharmProjects\\pythonProject\\HOPN ug\\Form_testcases_screenshots\\form_testcase4.png"
+    driver.save_screenshot(path)
     message = driver.find_element(By.ID,"amount-error").text
     print(message)
     assert "Amount must be a positive number greater than 0." in message
 
 def test_blank_amount(driver,fill_and_submit_form):
     fill_and_submit_form(vendor="Test", amount=" ", date="15/06/2025", status="Paid")
-    driver.save_screenshot("form_testcase5.png")
+    path = "C:\\Users\\Paras\\PycharmProjects\\pythonProject\\HOPN ug\\Form_testcases_screenshots\\form_testcase5.png"
+    driver.save_screenshot(path)
     message = driver.find_element(By.ID, "amount-error").text
     print(message)
     assert "Amount cannot be empty." in message
 
 def test_valid_pastdate(driver,fill_and_submit_form):
     fill_and_submit_form(vendor="Test", amount="50", date="14/06/2025", status="Paid")
-    driver.save_screenshot("form_testcase6.png")
+    path = "C:\\Users\\Paras\\PycharmProjects\\pythonProject\\HOPN ug\\Form_testcases_screenshots\\form_testcase6.png"
+    driver.save_screenshot(path)
     message = driver.find_element(By.ID, "success-box").text
     print(message)
     assert "Form submitted successfully!" in message
 
 def test_invalid_futuredate(driver,fill_and_submit_form):
     fill_and_submit_form(vendor="Test", amount="50", date="16/06/2025", status="Paid")
-    driver.save_screenshot("form_testcase7.png")
+    path = "C:\\Users\\Paras\\PycharmProjects\\pythonProject\\HOPN ug\\Form_testcases_screenshots\\form_testcase7.png"
+    driver.save_screenshot(path)
     message = driver.find_element(By.ID, "date-error").text
     print(message)
     assert "Date cannot be in the future." in message
 
 def test_invalid_dateformat(driver,fill_and_submit_form):
     fill_and_submit_form(vendor="Test", amount="50", date="01/15/2025", status="Paid")
-    driver.save_screenshot("form_testcase8.png")
+    path = "C:\\Users\\Paras\\PycharmProjects\\pythonProject\\HOPN ug\\Form_testcases_screenshots\\form_testcase8.png"
+    driver.save_screenshot(path)
     message = driver.find_element(By.ID, "date-error").text
     print(message)
     assert "Date must be in dd/mm/yyyy format." in message
 
 def test_blank_date(driver,fill_and_submit_form):
     fill_and_submit_form(vendor="Test", amount="50", date=" ", status="Paid")
-    driver.save_screenshot("form_testcase9.png")
+    path = "C:\\Users\\Paras\\PycharmProjects\\pythonProject\\HOPN ug\\Form_testcases_screenshots\\form_testcase9.png"
+    driver.save_screenshot(path)
     message = driver.find_element(By.ID, "date-error").text
     print(message)
     assert "Date cannot be empty." in message
 
 def test_valid_status(driver,fill_and_submit_form):
     fill_and_submit_form(vendor="Test", amount="50", date="14/06/2025", status="Pending")
-    driver.save_screenshot("form_testcase10.png")
+    path = "C:\\Users\\Paras\\PycharmProjects\\pythonProject\\HOPN ug\\Form_testcases_screenshots\\form_testcase10.png"
+    driver.save_screenshot(path)
     message = driver.find_element(By.ID, "success-box").text
     print(message)
     assert "Form submitted successfully!" in message
 
 def test_all_fields_empty(driver,fill_and_submit_form):
     fill_and_submit_form(vendor=" ", amount=" ", date="", status="Paid")
-    driver.save_screenshot("form_testcase11.png")
+    path = "C:\\Users\\Paras\\PycharmProjects\\pythonProject\\HOPN ug\\Form_testcases_screenshots\\form_testcase11.png"
+    driver.save_screenshot(path)
     messages = ["Vendor name cannot be empty.","Amount cannot be empty.","Date cannot be empty."]
     message1 = driver.find_element(By.ID, "vendor-error").text
     message2 = driver.find_element(By.ID, "amount-error").text
@@ -89,14 +100,16 @@ def test_all_fields_empty(driver,fill_and_submit_form):
 
 def test_invalid_name_sql_injection(driver,fill_and_submit_form):
     fill_and_submit_form(vendor="admin' -- ", amount="50", date="14/06/2025", status="Paid")
-    driver.save_screenshot("form_testcase12.png")
+    path = "C:\\Users\\Paras\\PycharmProjects\\pythonProject\\HOPN ug\\Form_testcases_screenshots\\form_testcase12.png"
+    driver.save_screenshot(path)
     message = driver.find_element(By.CLASS_NAME, "error").text
     print(message)
     assert "Vendor name must use letters and spaces only." in message
 
 def test_nonnumeric_amount(driver,fill_and_submit_form):
     fill_and_submit_form(vendor="Test",amount="50agbjh",date="14/06/2025",status="Paid")
-    driver.save_screenshot("form_testcase13.png")
+    path = "C:\\Users\\Paras\\PycharmProjects\\pythonProject\\HOPN ug\\Form_testcases_screenshots\\form_testcase13.png"
+    driver.save_screenshot(path)
     message = driver.find_element(By.ID,"amount-error").text
     print(message)
     assert "Amount must be a positive number greater than 0." in message
